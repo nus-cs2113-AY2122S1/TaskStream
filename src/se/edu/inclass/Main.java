ffd4d29f563846db75b061b9818ab56e8f4fe2b9
WaiKit-nus <waikit.lau98@gmail.com>
2021-09-17 17:24:10 +0800

    Commit all functions

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,10 +16,11 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
+        /*System.out.println("Printing deadlines");
+        printDeadlines(tasksData);*/
+        printDeadlinesUsingStream(tasksData);
 
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines: " + countDeadLinesUsingStream(tasksData));
 
     }
 
@@ -30,6 +31,11 @@ public class Main {
                 count++;
             }
         }
+        return count;
+    }
+
+    private static int countDeadLinesUsingStream(ArrayList<Task> tasks){
+        int count = (int) tasks.stream().filter((t)-> t instanceof Deadline).count();
         return count;
     }
 

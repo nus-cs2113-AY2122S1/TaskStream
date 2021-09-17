f8faa553a055a08d4858273c2ead543a95c285bd
DESKTOP-G7UTADU\xk_20 <xkisxk@gmail.com>
2021-09-17 17:10:52 +0800

    Added streams method

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -21,6 +21,7 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printDataWithStreams(tasksData);
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -34,9 +35,28 @@ public class Main {
     }
 
     public static void printData(ArrayList<Task> tasksData) {
+        System.out.println("Printing data by looping: ");
         for (Task t : tasksData) {
             System.out.println(t);
         }
+    }
+
+    public static void printDataWithStreams(ArrayList<Task> tasksData) {
+        tasksData.stream() // convert data to stream
+                .forEach(System.out::println); // terminal operator
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        tasks.stream() // convert data to stream
+                .filter((t) -> t instanceof Deadline)
+                .forEach(System.out::println); // terminal operator
+    }
+
+    private static int countDeadlinesWithStream(ArrayList<Task> tasksData) {
+        int count = (int) tasksData.stream()
+                .filter((t) -> t instanceof Deadline)
+                .count();
+        return count;
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
